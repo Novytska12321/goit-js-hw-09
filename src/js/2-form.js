259859fc1b18f7cf61d0e61f2eb0e03e787d1519
Novytska12321 <novytska@seznam.cz>
2024-08-20 21:12:11 +0200

    add all

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,50 @@
 console.log("form");
+let formData = {
+    email:  "",
+    message: "",
+}
+const formEl = document.querySelector('.feedback-form');
+const fillFormFields = () => {
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state')); 
+    if (formDataFromLS === null) {
+        return;
+    }
+    formData = formDataFromLS;
+    for (const key in formDataFromLS) {
+        if (formDataFromLS.hasOwnProperty(key)) {
+            formEl.elements[key].value = formDataFromLS[key];
+            
+        }
+    }
+    
+ };
+fillFormFields();
+const onFormElInput = event => {
+    
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    
+    
+    formData[fieldName] = fieldValue;
+    
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    
+   
+
+}
+const onFormElSubmit = event => { 
+    if (!formData.email || !formData.message) {
+        alert("Fill please all fields");
+        return;
+    }
+
+   
+    console.log(formData);
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem('feedback-form-state');
+    
+};
+formEl.addEventListener('input', onFormElInput);
+formEl.addEventListener('submit', onFormElSubmit);
 
